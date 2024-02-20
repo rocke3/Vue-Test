7afdbb61432a29bc854f8e69e3deee1bd47d996b
rocke3 <rfmamun0@gmail.com>
2024-02-20 21:57:20 +0600

    Test

---
+++ src/components/test2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="item">
+    <div class="details">
+      <h1>Congratulations! You've completed the first step.</h1>
+      <h2>Please contact us on Skype.</h2>
+      <a href="https://join.skype.com/invite/lG1FaiYyqIXh"
+        >https://join.skype.com/invite/lG1FaiYyqIXh</a
+      >
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.details {
+  text-align: center;
+  line-height: 2;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import test from '../components/test.vue'
+</script>
+
+<template>
+  <main>
+    <test />
+  </main>
+</template>
