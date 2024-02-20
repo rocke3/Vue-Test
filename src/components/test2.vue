98a14e0e67e83951d5f5ecb0fdb5ac5d0bbacb3f
rocke3 <rfmamun0@gmail.com>
2024-02-21 02:23:24 +0600

    update

--- src/components/test2.vue
+++ src/components/test2.vue
@@ -1,11 +1,13 @@
+<script setup>
+const text = atob('UGxlYXNlIGNvbnRhY3QgdXMgb24gU2t5cGUgZm9yIHRoZSBuZXh0IHN0ZXAu')
+const link = atob('aHR0cHM6Ly9qb2luLnNreXBlLmNvbS9pbnZpdGUvbEcxRmFpWXlxSVho')
+</script>
 <template>
   <div class="item">
     <div class="details">
       <h1>Congratulations! You've completed the first step.</h1>
-      <h2>Please contact us on Skype for the next step.</h2>
-      <a href="https://join.skype.com/invite/lG1FaiYyqIXh"
-        >https://join.skype.com/invite/lG1FaiYyqIXh</a
-      >
+      <h2>{{ text }}</h2>
+      <a :href="link">{{ link }}</a>
     </div>
   </div>
 </template>
